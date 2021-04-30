8179e39027c9e61a9fd543c2c1c1d784b7d99bb1
Abu <buang.h@ciraxes.com>
2021-04-30 18:43:45 +0800

    🎉  ADD: HomeScreen and Nav components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './components/HomeScreen';
 
 function App() {
   return (
     <div className='app'>
-      <h1>Lets build Netflix Clone 🚀🚀🚀</h1>
+      <HomeScreen />
     </div>
   );
 }
