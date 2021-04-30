7ec2f691eeb50738205f7f3f03ade5e3003c0e40
Abu <buang.h@ciraxes.com>
2021-04-30 18:00:53 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBrQQ9JZLlkyEIH-yO9g1jKDJPQpyprGYQ',
+  authDomain: 'netflix-clone-157be.firebaseapp.com',
+  projectId: 'netflix-clone-157be',
+  storageBucket: 'netflix-clone-157be.appspot.com',
+  messagingSenderId: '978147137370',
+  appId: '1:978147137370:web:a37b1a7f90d09f1481c563',
+};
